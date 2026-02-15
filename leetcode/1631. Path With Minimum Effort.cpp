843d3baaaf3173c37aa9f727e7211854e78a9944
ATUL KUMAR <atulsharma98535@gmail.com>
2026-02-15 10:01:12 +0530

    Dijsktra way to implement

--- leetcode/1631. Path With Minimum Effort.cpp
+++ leetcode/1631. Path With Minimum Effort.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define ll long long
 #define endl "\n"
 
+/*
+way 1
 int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
@@ -52,6 +54,51 @@ int minimumEffortPath(vector<vector<int>>& heights) {
         return dist[n-1][m-1];
 }
 
+*/
+
+void pushInHeap(priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>& pq, int i, int j , int row, int col,int effort,vector<vector<int>>& heights,vector<vector<bool>>& vis){
+
+        if(i+1 < row && vis[i+1][j] == false){
+              int newEffort = max(effort,abs(heights[i][j]-heights[i+1][j]));
+              pq.push({newEffort,{i+1,j}});
+        }
+         if(i-1 >= 0 && vis[i-1][j] == false){
+              int newEffort = max(effort,abs(heights[i][j]-heights[i-1][j]));
+              pq.push({newEffort,{i-1,j}});
+        }
+         if(j+1 < col && vis[i][j+1] == false){
+              int newEffort = max(effort,abs(heights[i][j]-heights[i][j+1]));
+              pq.push({newEffort,{i,j+1}});
+        }
+         if(j-1 >= 0 && vis[i][j-1] == false){
+              int newEffort = max(effort,abs(heights[i][j]-heights[i][j-1]));
+              pq.push({newEffort,{i,j-1}});
+        }
+
+}
+
+int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int row = heights.size(),col = heights[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}});
+        vector<vector<bool>>vis(row,vector<bool>(col,false));
+        
+        
+        while(pq.size()){
+            auto [effort, pos] = pq.top();
+            auto [i, j] = pos;
+            pq.pop();
+            if(vis[i][j]) continue;
+            if(i == row -1 && j == col-1) return effort;
+
+            vis[i][j] = true;
+            pushInHeap(pq,i,j,row,col,effort,heights,vis);
+        }
+        return -1;
+}
+
+   
 
 void solve() {
     vector<vector<int>>heights;
