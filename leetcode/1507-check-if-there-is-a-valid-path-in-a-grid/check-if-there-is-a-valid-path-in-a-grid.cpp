a16bb9283fd170fcc45b8d1d288f96168cc7daf8
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-04-27 17:30:19 +0530

    Time: 28 ms (43.95%) | Memory: 72.8 MB (16.53%) - LeetSync

--- leetcode/1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
+++ leetcode/1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
-    
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
+        vector<vector<int>> opens = {
+            {},         
+            {2, 3},     
+            {0, 1},   
+            {2, 1},     
+            {1, 3},     
+            {2, 0},    
+            {0, 3}      
+        };
+
+         vector<int> opposite = {1, 0, 3, 2};  
+            
     bool hasValidPath(vector<vector<int>>& grid) {
 
         int row = grid.size() , col = grid[0].size();
@@ -8,65 +21,72 @@ public:
         return dfs(0,0,row,col,vis,grid);
         
     }
-    bool dfs(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid ){
-           if(i < 0 || i >= r || j < 0 || j >= c) return false;
-           if(i == r - 1 && j == c - 1) return true;
-           if(vis[i][j]) return false;
-           vis[i][j] = true;
-
-           bool res = false;
-
-           if(grid[i][j] == 1) res = res || callLeft(i,j,r,c,vis,grid) || callRight(i,j,r,c,vis,grid);
-           else if(res == false && grid[i][j] == 2) res = res || callUp(i,j,r,c,vis,grid) || callDown(i,j,r,c,vis,grid);
-           else if(res == false && grid[i][j] == 3) res = res || callLeft(i,j,r,c,vis,grid) || callDown(i,j,r,c,vis,grid);
-           else if(res == false && grid[i][j] == 4) res = res || callDown(i,j,r,c,vis,grid) || callRight(i,j,r,c,vis,grid);
-           else if(res == false && grid[i][j] == 5) res = res || callLeft(i,j,r,c,vis,grid) || callUp(i,j,r,c,vis,grid);
-           else if(res == false && grid[i][j] == 6) res = res || callUp(i,j,r,c,vis,grid) || callRight(i,j,r,c,vis,grid);
+    bool dfs(int i, int j, int r, int c,vector<vector<bool>>& vis, vector<vector<int>>& grid) {
+    
+            if (i < 0 || i >= r || j < 0 || j >= c) return false;
+            if (vis[i][j]) return false;
+            if (i == r-1 && j == c-1) return true;
             
-           vis[i][j] = false;
-           return res;
-    }
-    bool callLeft(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
-         if(j - 1 < 0 || travelValid(i,j,i,j-1,grid) == false) return false;
-         return dfs(i,j-1,r,c,vis,grid);
-    }
-    bool callRight(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
-        if(j + 1 >= c || travelValid(i,j,i,j+1,grid) == false) return false;
-        return dfs(i,j+1,r,c,vis,grid);
-    }
-    bool callUp(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
-        if(i - 1 < 0 || travelValid(i,j,i-1,j,grid) == false) return false;
-        return dfs(i-1,j,r,c,vis,grid);
-    }
-    bool callDown(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
-        if(i + 1 >= r || travelValid(i,j,i+1,j,grid) == false) return false;
-        return dfs(i+1,j,r,c,vis,grid);
-        
-    }
+            vis[i][j] = true;  
 
-    bool travelValid(int i1,int j1,int i2,int j2,vector<vector<int>>& grid){
-         if(j1+1 == j2){
-               if (grid[i1][j1] == 1 && (grid[i2][j2] == 1 || grid[i2][j2] == 3 ||grid[i2][j2] == 5)) return true;
-               if (grid[i1][j1] == 4 && (grid[i2][j2] == 1 || grid[i2][j2] == 3 ||grid[i2][j2] == 5)) return true;
-               if (grid[i1][j1] == 6 && (grid[i2][j2] == 1 || grid[i2][j2] == 3 ||grid[i2][j2] == 5)) return true;
-         }
-          if(j1-1 == j2){
-               if (grid[i1][j1] == 1 && (grid[i2][j2] == 1 || grid[i2][j2] == 4 || grid[i2][j2] == 6)) return true;
-               if (grid[i1][j1] == 3 && (grid[i2][j2] == 1 || grid[i2][j2] == 4 || grid[i2][j2] == 6)) return true;
-               if (grid[i1][j1] == 5 && (grid[i2][j2] == 1 || grid[i2][j2] == 4 || grid[i2][j2] == 6)) return true;
-         }
-         if(i1+1 == i2){
-               if (grid[i1][j1] == 2 && (grid[i2][j2] == 2 || grid[i2][j2] == 5 || grid[i2][j2] == 6)) return true;
-               if (grid[i1][j1] == 3 && (grid[i2][j2] == 2 || grid[i2][j2] == 5 || grid[i2][j2] == 6)) return true;
-               if (grid[i1][j1] == 4 && (grid[i2][j2] == 2 || grid[i2][j2] == 5 || grid[i2][j2] == 6)) return true;
-         }
-          if(i1-1 == i2){
-               if (grid[i1][j1] == 2 && (grid[i2][j2] == 2 || grid[i2][j2] == 4 || grid[i2][j2] == 3)) return true;
-               if (grid[i1][j1] == 5 && (grid[i2][j2] == 2 || grid[i2][j2] == 4 || grid[i2][j2] == 3)) return true;
-               if (grid[i1][j1] == 6 && (grid[i2][j2] == 2 || grid[i2][j2] == 4 || grid[i2][j2] == 3)) return true;
-         }
-         return false;
-    }
+            for (int dir : opens[grid[i][j]]) {
+                int ni = i + dx[dir];
+                int nj = j + dy[dir];
+                
+                if (ni < 0 || ni >= r || nj < 0 || nj >= c) continue;
+
+               
+                int need = opposite[dir];
+                vector<int>& neighborDirs = opens[grid[ni][nj]];
+                
+                if (find(neighborDirs.begin(), neighborDirs.end(), need) 
+                    != neighborDirs.end()) {
+                    if (dfs(ni, nj, r, c, vis, grid)) return true;
+                }
+            }
+            return false;
+}
+    // bool callLeft(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
+    //      if(j - 1 < 0 || travelValid(i,j,i,j-1,grid) == false) return false;
+    //      return dfs(i,j-1,r,c,vis,grid);
+    // }
+    // bool callRight(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
+    //     if(j + 1 >= c || travelValid(i,j,i,j+1,grid) == false) return false;
+    //     return dfs(i,j+1,r,c,vis,grid);
+    // }
+    // bool callUp(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
+    //     if(i - 1 < 0 || travelValid(i,j,i-1,j,grid) == false) return false;
+    //     return dfs(i-1,j,r,c,vis,grid);
+    // }
+    // bool callDown(int i , int j , int r , int c , vector<vector<bool>>& vis ,vector<vector<int>>& grid){
+    //     if(i + 1 >= r || travelValid(i,j,i+1,j,grid) == false) return false;
+    //     return dfs(i+1,j,r,c,vis,grid);
+        
+    // }
+
+    // bool travelValid(int i1,int j1,int i2,int j2,vector<vector<int>>& grid){
+    //      if(j1+1 == j2){
+    //            if (grid[i1][j1] == 1 && (grid[i2][j2] == 1 || grid[i2][j2] == 3 ||grid[i2][j2] == 5)) return true;
+    //            if (grid[i1][j1] == 4 && (grid[i2][j2] == 1 || grid[i2][j2] == 3 ||grid[i2][j2] == 5)) return true;
+    //            if (grid[i1][j1] == 6 && (grid[i2][j2] == 1 || grid[i2][j2] == 3 ||grid[i2][j2] == 5)) return true;
+    //      }
+    //       if(j1-1 == j2){
+    //            if (grid[i1][j1] == 1 && (grid[i2][j2] == 1 || grid[i2][j2] == 4 || grid[i2][j2] == 6)) return true;
+    //            if (grid[i1][j1] == 3 && (grid[i2][j2] == 1 || grid[i2][j2] == 4 || grid[i2][j2] == 6)) return true;
+    //            if (grid[i1][j1] == 5 && (grid[i2][j2] == 1 || grid[i2][j2] == 4 || grid[i2][j2] == 6)) return true;
+    //      }
+    //      if(i1+1 == i2){
+    //            if (grid[i1][j1] == 2 && (grid[i2][j2] == 2 || grid[i2][j2] == 5 || grid[i2][j2] == 6)) return true;
+    //            if (grid[i1][j1] == 3 && (grid[i2][j2] == 2 || grid[i2][j2] == 5 || grid[i2][j2] == 6)) return true;
+    //            if (grid[i1][j1] == 4 && (grid[i2][j2] == 2 || grid[i2][j2] == 5 || grid[i2][j2] == 6)) return true;
+    //      }
+    //       if(i1-1 == i2){
+    //            if (grid[i1][j1] == 2 && (grid[i2][j2] == 2 || grid[i2][j2] == 4 || grid[i2][j2] == 3)) return true;
+    //            if (grid[i1][j1] == 5 && (grid[i2][j2] == 2 || grid[i2][j2] == 4 || grid[i2][j2] == 3)) return true;
+    //            if (grid[i1][j1] == 6 && (grid[i2][j2] == 2 || grid[i2][j2] == 4 || grid[i2][j2] == 3)) return true;
+    //      }
+    //      return false;
+    // }
 };
 
 /*            -1
