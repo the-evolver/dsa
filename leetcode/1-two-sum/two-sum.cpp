1442828bb9ee3a59708ae20aab99fd26dd26341e
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-06-19 20:46:18 +0530

    Time: 3 ms (67.06%) | Memory: 14.7 MB (54.52%) - LeetSync

--- leetcode/1-two-sum/two-sum.cpp
+++ leetcode/1-two-sum/two-sum.cpp
@@ -4,25 +4,40 @@ public:
         
 
         // Solution 1 o(nlogn)
-        vector<pair<int,int>> pnums;
-        for(int i = 0; i < nums.size();i++){
-            pnums.push_back({nums[i],i});
-        }
+        // vector<pair<int,int>> pnums;
+        // for(int i = 0; i < nums.size();i++){
+        //     pnums.push_back({nums[i],i});
+        // }
 
-        sort(pnums.begin(),pnums.end());
+        // sort(pnums.begin(),pnums.end());
 
-        int i = 0 , j = pnums.size()-1;
-        while(i < j){
-            int curr = pnums[i].first + pnums[j].first;
-            if(curr == target){
-                return {pnums[i].second,pnums[j].second};
-            }else if(curr > target){
-                 j--;
-            }else{
-                i++;
-            }
+        // int i = 0 , j = pnums.size()-1;
+        // while(i < j){
+        //     int curr = pnums[i].first + pnums[j].first;
+        //     if(curr == target){
+        //         return {pnums[i].second,pnums[j].second};
+        //     }else if(curr > target){
+        //          j--;
+        //     }else{
+        //         i++;
+        //     }
+        // }
+        // return {-1,-1}; // will never come here..
+
+        // Solution 2
+
+        unordered_map<int,int> ump;
+        for(int i = 0 ; i < nums.size();i++){
+             int curr = nums[i];
+             if(ump.find(target-curr) != ump.end()){
+                return {i,ump[target-curr]};
+             }else{
+                ump[curr] = i;
+             }
+
         }
-        return {-1,-1}; // will never come here..
+        return {-1,-1};
+
 
     }
 };
