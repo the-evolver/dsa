bfbdc60e128cc7779f0c7a4335a2b00b2813b92b
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-06-21 23:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (10.45%) - LeetSync

--- leetcode/740-delete-and-earn/delete-and-earn.cpp
+++ leetcode/740-delete-and-earn/delete-and-earn.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
 
-        int n = 10e4 +1;
+        int n = 10000 + 1;
         vector<int> arr(n,0);
+        vector<int> dp(n,0);
+
         for(int x : nums) arr[x] += x;
 
-        vector<vector<int>> dp(n,vector<int>(2,0));
-        for(int i = 0 ; i < dp.size();i++){
-             dp[i][0] = arr[i] + (i - 2 >= 0 ? max(dp[i-2][0],dp[i-2][1]) : 0);
-             dp[i][1] = i - 1 >= 0 ? max(dp[i-1][0],dp[i-1][1]) :0;
-             
+        for(int i = 1 ; i < n ; i++){
+            dp[i] = max(arr[i] + (i-2 >= 0 ? dp[i-2] : 0),dp[i-1]);
         }
-
-        return max(dp[n-1][0],dp[n-1][1]);
-        
+        return dp[n-1];
     }
 };
