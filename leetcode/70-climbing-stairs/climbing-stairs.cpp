5a5f566f0641c6b78f14dbbe07ca450dcb79d074
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-08-14 15:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (39.05%) - LeetSync

---
+++ leetcode/70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        //if(n <= 1) return 1;
+        //return climbStairs(n-1) + climbStairs(n-2);
+        if(n <= 2) return n;
+        vector<int> arr(n+1,0);
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3 ; i <=n ; i++){
+            arr[i] = arr[i-1]+ arr[i-2];
+        }
+        return arr[n];
+    }
+};
