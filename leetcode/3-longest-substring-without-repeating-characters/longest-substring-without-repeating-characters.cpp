e208dcf0021388b39e956a2f07ee08d4a98823ca
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-08-17 13:16:01 +0530

    Time: 59 ms (20.69%) | Memory: 19.3 MB (23.66%) - LeetSync

---
+++ leetcode/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        unordered_map<char,int> mp;
+        int left = 0,right = 0;
+        int curr = 0,maxi = 0;
+        while(right < s.length()){
+            char c = s[right];
+             mp[c]++;
+             while(left <= right && mp[c] > 1){
+                    mp[s[left]]--;
+                    left++;
+             }
+             
+             curr = right - left +1;
+             maxi = max(curr,maxi);
+            right++;
+
+        }
+        return maxi;
+    }
+};
