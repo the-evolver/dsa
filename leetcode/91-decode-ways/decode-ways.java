a0dcf4a191f9139e5530ec79b53c78bf0e415e84
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-08-20 21:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (33.72%) - LeetSync

--- leetcode/91-decode-ways/decode-ways.java
+++ leetcode/91-decode-ways/decode-ways.java
@@ -1,27 +1,25 @@
 class Solution {
     Integer[] dp;
-
     public int numDecodings(String s) {
+
         dp = new Integer[s.length()];
-        return solve(s.toCharArray(), 0);
+        return solve(s.toCharArray(),0);
+        
     }
 
-    private int solve(char[] str, int i) {
-        if (i == str.length) return 1;
-        if (str[i] == '0') return 0;
+    public int solve(char[] arr,int i){
+        if(i == arr.length) return 1 ; // because we reached end while decoding so whole process is 1 way of decoding
+        if(arr[i] == '0') return 0; // not possible saar
+        
+        if(dp[i] != null) return dp[i]; // already calculated the amswer sarrr
 
-        if (dp[i] != null) return dp[i];
+        int ways = solve(arr,i+1); // one digit way .. no 0 here so it can be acted as a digit
 
-        int ways = solve(str, i + 1);
-
-        if (i + 1 < str.length) {
-            int num = (str[i] - '0') * 10 + (str[i + 1] - '0');
-
-            if (num <= 26) {
-                ways += solve(str, i + 2);
-            }
+        if(i + 1 < arr.length){
+             int num = (arr[i] - '0')*10 + (arr[i+1] - '0');
+             if(num <= 26) ways += solve(arr,i+2);
         }
-
+        
         return dp[i] = ways;
     }
 }
