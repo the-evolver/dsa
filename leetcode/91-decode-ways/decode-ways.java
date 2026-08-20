f757a58fe3f836e9b0a2b6748d00b88ac7b97545
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-08-20 21:36:46 +0530

    Time: 1 ms (79.97%) | Memory: 42.9 MB (76.95%) - LeetSync

--- leetcode/91-decode-ways/decode-ways.java
+++ leetcode/91-decode-ways/decode-ways.java
@@ -1,25 +1,45 @@
 class Solution {
-    Integer[] dp;
+    //Integer[] dp;
     public int numDecodings(String s) {
 
-        dp = new Integer[s.length()];
-        return solve(s.toCharArray(),0);
-        
-    }
+        // dp = new Integer[s.length()];
+        // return solve(s.toCharArray(),0);
 
-    public int solve(char[] arr,int i){
-        if(i == arr.length) return 1 ; // because we reached end while decoding so whole process is 1 way of decoding
-        if(arr[i] == '0') return 0; // not possible saar
-        
-        if(dp[i] != null) return dp[i]; // already calculated the amswer sarrr
+        // Iterative way
 
-        int ways = solve(arr,i+1); // one digit way .. no 0 here so it can be acted as a digit
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+        char[] str = s.toCharArray();
+        for(int i = str.length - 1 ; i >= 0 ;i--){
 
-        if(i + 1 < arr.length){
-             int num = (arr[i] - '0')*10 + (arr[i+1] - '0');
-             if(num <= 26) ways += solve(arr,i+2);
+             if(str[i] == '0') continue;
+             dp[i] +=  dp[i+1];
+
+             if(i+1 < str.length){
+                 int ifTwoDig = (str[i] - '0') * 10 + (str[i+1] - '0');
+
+                 if(ifTwoDig <= 26)
+                    dp[i] += dp[i+2];
+             } 
         }
+          
+          return dp[0];
         
-        return dp[i] = ways;
     }
+
+    // public int solve(char[] arr,int i){
+    //     if(i == arr.length) return 1 ; // because we reached end while decoding so whole process is 1 way of decoding
+    //     if(arr[i] == '0') return 0; // not possible saar
+        
+    //     if(dp[i] != null) return dp[i]; // already calculated the amswer sarrr
+
+    //     int ways = solve(arr,i+1); // one digit way .. no 0 here so it can be acted as a digit
+
+    //     if(i + 1 < arr.length){
+    //          int num = (arr[i] - '0')*10 + (arr[i+1] - '0');
+    //          if(num <= 26) ways += solve(arr,i+2);
+    //     }
+        
+    //     return dp[i] = ways;
+    // }
 }
