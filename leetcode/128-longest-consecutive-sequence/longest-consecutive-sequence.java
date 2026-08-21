2817c9ec33ebc1a584348252e92a03d36fb8afbe
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-08-21 15:14:01 +0530

    Time: 64 ms (5.09%) | Memory: 96.6 MB (5.01%) - LeetSync

--- leetcode/128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ leetcode/128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,26 +1,21 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
 
-        // for passing in o(n) we need to cnt the largest consecutive elements till i in the array when at i so that at n-1 we have the answer ...
+        Map<Integer,Integer> mp = new HashMap<>();
+        int maxi = 0;
 
-        // any number x can contribute in two ways either it has neighbours else 1 if neighbours x will contribute like lce(x-1) + 1 + lce(x+1)
-        // also we dont need to update the whole interval because at the 
-        // end we need only interval end values because mid interval will have counted result so bigger result can come from intervals outside valuse which will need the intervals end valuse to calculate..
-
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int maxL = 0;
         for(int num : nums){
-            if(hm.containsKey(num)) continue;
+             if(mp.containsKey(num)) continue;
 
-            int left = hm.getOrDefault(num-1,0);
-            int right = hm.getOrDefault(num+1,0);
+             int left = mp.getOrDefault(num-1,0);
+             int right = mp.getOrDefault(num+1,0);
 
-            int newV = left + 1 + right;
-            maxL = maxL < newV ? newV : maxL;
-            hm.put(num-left,newV);
-            hm.put(num+right,newV);
-            hm.put(num,newV);
+             mp.put(num,left + right + 1) ;
+             mp.put(num-left,left + right + 1) ;
+             mp.put(num+right,left + right + 1) ;
+             
+             maxi = Integer.max(maxi,mp.get(num));
         }
-        return maxL;
+        return maxi;
     }
 }
