7ea7dc4d66369a3254754a12fed34db32360db6f
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2026-08-21 10:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (97.46%) - LeetSync

--- leetcode/198-house-robber/house-robber.java
+++ leetcode/198-house-robber/house-robber.java
@@ -1,18 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
 
-        int n = nums.length;
-        int[][] dp = new int[n][2];
-        dp[0][0] = nums[0];  // dp[i][0] = store max money till ith house looting ith house
-        dp[0][1] = 0;        // max money till ith house without looting ith house...
+        int[][] dp = new int[nums.length][2];
+        dp[0][0] = nums[0];
+        dp[0][1] = 0;
 
-        for(int i = 1 ; i < n ; i++){
-             dp[i][0] = nums[i] + dp[i-1][1];
+        for(int i = 1 ; i < nums.length;i++){
+             dp[i][0] = dp[i-1][1] + nums[i] ;
              dp[i][1] = Integer.max(dp[i-1][0],dp[i-1][1]);
-             
         }
-
-        return Integer.max(dp[n-1][0],dp[n-1][1]);
         
+        return Integer.max(dp[nums.length-1][0],dp[nums.length-1][1]);
     }
 }
